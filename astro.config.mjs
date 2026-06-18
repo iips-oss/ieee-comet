c5ef801f8485ff4de6a0bd0dd97eac8006fea19f
cxinu <cxinu.main@protonmail.com>
2026-06-18 14:28:23 +0530

    astro urls and gh action deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://iips-oss.github.io',
+  base: '/ieee-comet',
   vite: {
     plugins: [tailwindcss()]
   }
